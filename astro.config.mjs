d3be39c7bb56d7d0c6a6fcbbd10feb7ffa9d365e
Roharui <jack555586@gmail.com>
2022-12-24 21:32:37 +0900

    feat: 검색 페이지 구현

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
 
 export default defineConfig({
   output: "server",
   adapter: vercel(),
+  integrations: [react()],
 });
